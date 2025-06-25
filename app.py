bfd7b09ea7d6756b8dd600fafbe440bfb330f860
fernandallobao <fernandafcclobao@gmail.com>
2025-06-25 14:19:48 -0300

    corrigindo o botao

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    visoes = ['Robô', 'Lousa', 'Lousa']
+    visoes = ['Robô', 'Lousa', 'Libras']
     cards_data = get_cards()
     return render_template('index.html', visoes=visoes, cards=cards_data)
 
